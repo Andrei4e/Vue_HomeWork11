dc958beedf0cb900434d7c24b58c2918dc49f9b6
Andrei <Andrei@mail.ru>
2024-03-14 21:39:12 +0300

    готовое дз (урок 11)

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <HomePage /> -->
+    <!-- <BlogPage /> -->
+    <!-- <BlogDetailsPage /> -->
+    <!-- <ProductDetails /> -->
+    <ProjectPage />
+  </div>
+</template>
+
+<script>
+// import HomePage from "./components/HomePage.vue";
+//import BlogPage from "./components/BlogPage.vue";
+//import BlogDetailsPage from "./components/BlogDetailsPage.vue";
+//import ProductDetails from "./components/ProductDetails.vue";
+import ProjectPage from "./components/ProjectPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    // HomePage,
+    //BlogPage,
+    //BlogDetailsPage,
+    //ProductDetails,
+    ProjectPage
+  },
+};
+</script>
+
+<style>
+
+</style>
